42144e4b964eaefd609ee72b368b34390c432b29
SamuelNittala <nittalasamuel7@gmail.com>
2021-07-17 20:43:52 +0530

    Try block

--- db.py
+++ db.py
@@ -10,7 +10,10 @@ engine = create_engine(os.getenv('URI'))
 def insert_into_table(agent,ability,side,map,area,url):
     area_tags = area.split(',')
     insert_query = f"insert into {agent}(ability,side,map,area,url)values({ability},{side},{map},Array{area_tags},{url})"
-    pd.read_sql(insert_query,engine)
+    try:
+        pd.read_sql(insert_query,engine)
+    except:
+        print('Insertion success')
 
 
 
